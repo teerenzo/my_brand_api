c01e8b30793526b5aefd49ec45541bf5f5927d1a
Andela Rwanda <andelarwanda@Andelas-MacBook-Pro.local>
2022-12-19 17:14:24 +0200

    passport

--- src/middleware/authoMiddlewares.js
+++ src/middleware/authoMiddlewares.js
@@ -1,5 +1,5 @@
-const jwt = require('jsonwebtoken')
-const User = require('../models/userModel')
+import jwt from 'jsonwebtoken'
+import User from '../models/userModel'
 const tokenSecret = "my-token-secret"
 
 exports.protect = (req, res, next) => {
