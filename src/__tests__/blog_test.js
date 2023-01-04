a4397c47e1c3cf0571b22bd51f25706553b1c0a0
teerenzo <renzahoemmanuel8@gmail.com>
2023-01-04 21:01:52 +0200

    tests

--- src/__tests__/blog_test.js
+++ src/__tests__/blog_test.js
@@ -74,62 +74,62 @@ describe('Testing Blog routes', () => {
     
 
     })
-    // ,
-    // it('should get all blog articles.',async()=>{
+    ,
+    it('should get all blog articles.',async()=>{
 
-    //     const res=await chai.request(app).get('/api/articles/')
-    //     expect(res.status).to.be.equal(200);
+        const res=await chai.request(app).get('/api/articles/')
+        expect(res.status).to.be.equal(200);
        
-    // }),
-    // it('should get one blog article by id',async()=>{
-    //     const article=await chai.request(app).get('/api/articles/')
-    //     const id=article.body[0]._id
-    //     const res=await chai.request(app).get(`/api/articles/${id}`)
-    //     expect(res.status).to.be.equal(200);
-    // }),
-    // it('should update blog article',async()=>{
-    //     const signUp=await chai.request(app).post('/api/account/signUp').send((tester));
-    //     const adminSignin=await chai.request(app).post('/api/account/login').send(admin)
-    //     const token = `Bearer ${adminSignin.body.user.token}`;
-    //     const res1=await chai.request(app).post('/api/articles/add').send(testingData).set('Authorization', token)
-    //     const article=await chai.request(app).get('/api/articles/')
-    //     const id=article.body[0]._id
+    }),
+    it('should get one blog article by id',async()=>{
+        const article=await chai.request(app).get('/api/articles/')
+        const id=article.body[0]._id
+        const res=await chai.request(app).get(`/api/articles/${id}`)
+        expect(res.status).to.be.equal(200);
+    }),
+    it('should update blog article',async()=>{
+        const signUp=await chai.request(app).post('/api/account/signUp').send((tester));
+        const adminSignin=await chai.request(app).post('/api/account/login').send(admin)
+        const token = `Bearer ${adminSignin.body.user.token}`;
+        const res1=await chai.request(app).post('/api/articles/add').send(testingData).set('Authorization', token)
+        const article=await chai.request(app).get('/api/articles/')
+        const id=article.body[0]._id
        
-    //     const res=await chai.request(app).put(`/api/articles/${id}/update`).send(testingDataUpdate).set('Authorization', token)
+        const res=await chai.request(app).put(`/api/articles/${id}/update`).send(testingDataUpdate).set('Authorization', token)
    
-    //     expect(res.status).to.be.equal(200);
-    // }),
-    // it('should delete blog article',async()=>{
-    //     const signUp=await chai.request(app).post('/api/account/signUp').send((tester));
-    //     const adminSignin=await chai.request(app).post('/api/account/login').send(admin)
-    //     const token = `Bearer ${adminSignin.body.user.token}`;
-    //     const article=await chai.request(app).get('/api/articles/')
+        expect(res.status).to.be.equal(200);
+    }),
+    it('should delete blog article',async()=>{
+        const signUp=await chai.request(app).post('/api/account/signUp').send((tester));
+        const adminSignin=await chai.request(app).post('/api/account/login').send(admin)
+        const token = `Bearer ${adminSignin.body.user.token}`;
+        const article=await chai.request(app).get('/api/articles/')
 
-    //     const id=article.body[0]._id
-    //     const res=await chai.request(app).delete(`/api/articles/${id}`).set('Authorization', token)
+        const id=article.body[0]._id
+        const res=await chai.request(app).delete(`/api/articles/${id}`).set('Authorization', token)
       
-    //     expect(res.status).to.be.equal(200);
+        expect(res.status).to.be.equal(200);
 
   
-    // }),
-    // it('should comment on blog article',async()=>{
-    //     const signUp=await chai.request(app).post('/api/account/signUp').send((tester));
-    //     const adminSignin=await chai.request(app).post('/api/account/login').send(admin)
-    //     const token = `Bearer ${adminSignin.body.user.token}`;
-    //     const article=await chai.request(app).get('/api/articles/')
-    //     const id=article.body[0]._id
-    //     const res=await chai.request(app).post(`/api/articles/${id}/comment/`).send(testingDataUpdate).set('Authorization', token).send({"comment":"that content is very helpful thanks"})
-    //     expect(res.status).to.be.equal(200);
+    }),
+    it('should comment on blog article',async()=>{
+        const signUp=await chai.request(app).post('/api/account/signUp').send((tester));
+        const adminSignin=await chai.request(app).post('/api/account/login').send(admin)
+        const token = `Bearer ${adminSignin.body.user.token}`;
+        const article=await chai.request(app).get('/api/articles/')
+        const id=article.body[0]._id
+        const res=await chai.request(app).post(`/api/articles/${id}/comment/`).send(testingDataUpdate).set('Authorization', token).send({"comment":"that content is very helpful thanks"})
+        expect(res.status).to.be.equal(200);
 
-    // }),
-    // it('should like on blog article',async()=>{
-    //     const signUp=await chai.request(app).post('/api/account/signUp').send((tester));
-    //     const adminSignin=await chai.request(app).post('/api/account/login').send(admin)
-    //     const token = `Bearer ${adminSignin.body.user.token}`;
-    //     const article=await chai.request(app).get('/api/articles/')
-    //     const id=article.body[0]._id
-    //     const res=await chai.request(app).post(`/api/articles/${id}/like`).send(testingDataUpdate).set('Authorization', token).send({"article_id":id})
-    //     expect(res.status).to.be.equal(200);
+    }),
+    it('should like on blog article',async()=>{
+        const signUp=await chai.request(app).post('/api/account/signUp').send((tester));
+        const adminSignin=await chai.request(app).post('/api/account/login').send(admin)
+        const token = `Bearer ${adminSignin.body.user.token}`;
+        const article=await chai.request(app).get('/api/articles/')
+        const id=article.body[0]._id
+        const res=await chai.request(app).post(`/api/articles/${id}/like`).send(testingDataUpdate).set('Authorization', token).send({"article_id":id})
+        expect(res.status).to.be.equal(200);
 
-    // })
+    })
 })
